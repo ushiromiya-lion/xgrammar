9013c12dc90d99138868a4fc66b5a298555acb12
Yuchuan <yuchuan.7streams@gmail.com>
2025-12-30 17:23:48 +0800

    remove hasher.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- cpp/grammar_functor.h
+++ cpp/grammar_functor.h
@@ -18,6 +18,7 @@
 #include "grammar_builder.h"
 #include "grammar_impl.h"
 #include "support/memory_size.h"
+#include "support/thread_safe_cache.h"
 #include "xgrammar/grammar.h"
 
 namespace xgrammar {
@@ -390,77 +391,6 @@ class GrammarOptimizer {
 class GrammarFSMHasher {
  public:
   static void Apply(Grammar* grammar);
-};
-
-/*!
- * \brief Store the crossing cache for different grammars.
- * \param max_cache_size The maximum size of the cache numbers.
- * \details LRU algorithm is implemented.
- */
-class CrossingCacheManager {
- public:
-  std::optional<AdaptiveTokenMask> GetCache(
-      const uint64_t& fsm_hash, int32_t fsm_new_node_id, const uint64_t& tokenizer_hash
-  );
-  bool AddCache(
-      const uint64_t& fsm_hash,
-      int32_t fsm_new_node_id,
-      const uint64_t& tokenizer_hash,
-      const AdaptiveTokenMask& token_mask
-  );
-  bool AddCache(
-      const uint64_t& fsm_hash,
-      int32_t fsm_new_node_id,
-      const uint64_t& tokenizer_hash,
-      AdaptiveTokenMask&& token_mask
-  );
-  CrossingCacheManager(size_t max_cache_memory_size = 1e7)
-      : crossing_cache_manager_impl_(max_cache_memory_size) {
-    XGRAMMAR_CHECK(max_cache_memory_size != 0);
-  }
-
-  void ClearCache() { crossing_cache_manager_impl_.ClearCache(); }
-
-  friend size_t MemorySize(const CrossingCacheManager& manager) {
-    return MemorySize(manager.crossing_cache_manager_impl_);
-  }
-
- private:
-  class CrossingCacheManagerImpl {
-   public:
-    std::optional<AdaptiveTokenMask> GetCache(
-        const uint64_t& fsm_hash, int32_t fsm_new_node_id, const uint64_t& tokenizer_hash
-    );
-    bool AddCache(
-        const uint64_t& fsm_hash,
-        int32_t fsm_new_node_id,
-        const uint64_t& tokenizer_hash,
-        const AdaptiveTokenMask& token_mask
-    );
-    bool AddCache(
-        const uint64_t& fsm_hash,
-        int32_t fsm_new_node_id,
-        const uint64_t& tokenizer_hash,
-        AdaptiveTokenMask&& token_mask
-    );
-    CrossingCacheManagerImpl(size_t max_cache_memory_size = 1e7)
-        : max_cache_memory_size_(max_cache_memory_size) {}
-
-    void ClearCache();
-
-    friend size_t MemorySize(const CrossingCacheManagerImpl& manager) {
-      return manager.current_cache_memory_size_;
-    }
-
-   private:
-    std::mutex mutex_;
-    const size_t max_cache_memory_size_;
-    int64_t current_cache_memory_size_ = 0;
-    std::list<std::pair<std::tuple<uint64_t, int32_t, uint64_t>, AdaptiveTokenMask>> cache_list_;
-    std::unordered_map<std::tuple<uint64_t, int32_t, uint64_t>, decltype(cache_list_.begin())>
-        cache_;
-  };
-  CrossingCacheManagerImpl crossing_cache_manager_impl_;
 };
 
 }  // namespace xgrammar
